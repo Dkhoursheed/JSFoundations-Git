f109b5ac28ac85b2eb5d1cb45925ac3bdca24943
Dkhoursheed <Daloola.k@hotmail.com>
2020-10-18 20:32:38 +0300

    done

---
+++ repo/story.js
@@ -0,0 +1,8 @@
+let name = "Mshary";
+let age = 25;
+let hobby = "Meditation";
+console.log(
+  `Mr ${name}, in 33 years you will be ${
+    age + 33
+  }. By then you'll have years of ${hobby} practice`
+);
